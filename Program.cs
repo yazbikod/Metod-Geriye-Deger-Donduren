f82ef289b13305cf13463040fb4a7dafbbe98c9f
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 02:21:16 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int donenDeger = toplamaYap(6, 7);
+Console.WriteLine("Toplam = " + donenDeger);
+
+string donenisimsoyisim = isimsoyisimyazdir("Berk", "DURAN");
+Console.WriteLine("Benim adım : " + donenisimsoyisim);
+  }
+  
+  static int toplamaYap(int sayi1, int sayi2)
+{
+    int sonuc = sayi1 + sayi2;
+    return sonuc;
+}
+static string isimsoyisimyazdir(string isim, string soyisim)
+{
+    return isim + " " + soyisim;
+}
